9478dc5576f814097c45815f7e9093fa4e38b31c
Pai2Chen <zldlsr@163.com>
2021-12-19 12:03:45 +0800

    fix lwjgl jar path

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"io/fs"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -13,8 +14,15 @@ func main() {
 	proPath, _ := filepath.Abs(args[0])
 	basePath := filepath.Dir(proPath)
 
+	nClassPath := make([]string, 0)
+	filepath.Walk(basePath+"/lwjgl/", func(path string, info fs.FileInfo, err error) error {
+		if strings.Contains(info.Name(), "lwjgl") {
+			nClassPath = append(nClassPath, path)
+		}
+		return err
+	})
+
 	classPath := strings.Split(os.Getenv("CLASSPATH"), ":")
-	nClassPath := []string{basePath + "/lwjglfat.jar"}
 
 	for _, v := range classPath {
 		if !strings.Contains(v, "lwjgl") {
@@ -22,48 +30,9 @@ func main() {
 		}
 	}
 
-	/*for i := range args {
-		if strings.Contains(args[i], "-cp") {
-			classPath := strings.Split(args[i+1], ":")
-
-			for j := range classPath {
-				if strings.Contains(classPath[j], "lwjgl") {
-					classPath = append(classPath[:j], classPath[j+1:]...)
-				}
-			}
-
-			classPath = append(classPath, basePath+"lwjglfat.jar")
-			//println(strings.Join(classPath, " "))
-
-			args[i+1] = strings.Join(classPath, ":")
-
-			break
-		}
-	}*/
-
-	//println(strings.Join(args, " "))
-
-	//println(strings.Join(classPath, ":"))
-
 	cmd := exec.Command(args[1], args[2:]...)
 	cmd.Env = append(os.Environ(), "CLASSPATH="+strings.Join(nClassPath, ":"))
 	cmd.Stdout = os.Stdout
-
-	/*var err error
-
-	var wg sync.WaitGroup
-
-	go func() {
-		wg.Add(1)
-		err = cmd.Run()
-		wg.Done()
-	}()
-	//println(string(stdout.Bytes()))
-	wg.Wait()
-	if err != nil {
-		println(err.Error())
-		return
-	}*/
 
 	println(strings.Join(nClassPath, ":"))
 
