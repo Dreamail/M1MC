72997238510bd906764eab7a4f8ddeccddb2e7db
Dreamail <dreaillll@outlook.com>
2022-03-06 12:14:53 +0800

    auto download libraries

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module M1MC
 
 go 1.17
+
+require github.com/cavaliergopher/grab/v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"archive/zip"
+	"github.com/cavaliergopher/grab/v3"
+	"io"
 	"io/fs"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
 )
 
+var wg sync.WaitGroup
+
 func main() {
+	getLibs()
+
 	args := os.Args
 	proPath, _ := filepath.Abs(args[0])
 	basePath := filepath.Dir(proPath)
@@ -63,4 +74,142 @@ func main() {
 	}*/
 
 	cmd.Run()
+}
+
+func getLibs() {
+	// what i need to get is lwjgl lwjgl-glfw lwjgl-jemalloc lwjgl-tinyfd lwjgl-stb lwjgl-opengl lwjgl-openal(without native) openal objc
+	lwjgls := []string{"lwjgl", "lwjgl-glfw", "lwjgl-jemalloc", "lwjgl-tinyfd", "lwjgl-stb", "lwjgl-opengl", "lwjgl-openal"}
+	metaUrls := []string{"https://repo1.maven.org/maven2/ca/weblite/java-objc-bridge/maven-metadata.xml", "https://repo1.maven.org/maven2/org/lwjgl/lwjgl/maven-metadata.xml"}
+
+	lwjglVersion := ""
+	ObjCVersion := ""
+
+	/*client := http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			r.URL.Opaque = r.URL.Path
+			return nil
+		},
+	}*/
+
+	for _, v := range []string{"libraries", "natives", "temp"} {
+		err := os.Mkdir(v, 0770)
+		if err != nil {
+			if !strings.Contains(err.Error(), "file exists") {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	for _, v := range metaUrls {
+		resp, err := http.Get(v)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		xmlbytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		resp.Body.Close()
+
+		xmlstr := string(xmlbytes)
+		versionIndex := strings.Index(xmlstr, "<latest>") + 8
+		versionOutdex := strings.Index(xmlstr, "</latest>")
+
+		version := string([]rune(xmlstr)[versionIndex:versionOutdex])
+		if strings.Contains(v, "lwjgl") {
+			lwjglVersion = version
+		} else {
+			ObjCVersion = version
+		}
+	}
+
+	for _, v := range lwjgls { // process lwjgl
+		jarUrl := "https://repo1.maven.org/maven2/org/lwjgl/" + v + "/" + lwjglVersion + "/" + v + "-" + lwjglVersion + ".jar"
+		nativeUrl := "https://repo1.maven.org/maven2/org/lwjgl/" + v + "/" + lwjglVersion + "/" + v + "-" + lwjglVersion + "-natives-macos-arm64.jar"
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			_, err := grab.Get("libraries/", jarUrl)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}()
+
+		//if v == "lwjgl-openal" { // TODO using openal-soft, according to GameParrot/minecraft-mac-window-fix
+		//	continue
+		//}
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			resp, err := grab.Get("temp/", nativeUrl)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			jarZip, err := zip.OpenReader(resp.Filename)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, v := range jarZip.File {
+				if strings.Contains(v.Name, "dylib") {
+					dylibZip, err := v.Open()
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					dylib, err := os.Create("natives/" + strings.Split(v.Name, "/")[strings.Count(v.Name, "/")])
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					io.Copy(dylib, dylibZip)
+					dylibZip.Close()
+					dylib.Close()
+
+					break
+				}
+			}
+
+			jarZip.Close()
+		}()
+	}
+
+	wg.Add(1)
+	go func() { // process ojbc-bridge
+		defer wg.Done()
+
+		jarUrl := "https://repo1.maven.org/maven2/ca/weblite/java-objc-bridge/" + ObjCVersion + "/" + "java-objc-bridge-" + ObjCVersion + ".jar"
+
+		resp, err := grab.Get("temp/", jarUrl)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		jarZip, err := zip.OpenReader(resp.Filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		dylib, err := os.Create("natives/" + "libjcocoa.dylib")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		dylibZip, err := jarZip.Open("libjcocoa.dylib")
+
+		//TODO remove native in jar
+
+		io.Copy(dylib, dylibZip)
+		jarZip.Close()
+		dylib.Close()
+	}()
+
+	wg.Wait()
+	os.RemoveAll("temp/")
 }
